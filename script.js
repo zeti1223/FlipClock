fea3c97537f14901a552e00ce1a4b45260cf4571
Devesh Mani <deveshchallengers@gmail.com>
2025-09-24 00:34:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,9 @@ $(document).ready(function () {
   updateDate();
   setInterval(updateDate, 60000);
 
-  // Dynamically add the ClustrMaps script
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.id = 'clustrmaps';
-  script.src = '//clustrmaps.com/map_v2.js?d=FCEx_R1QbsHAfae0MaYTzeNjyHpWR2agwxSu1MTipw4&cl=ffffff&w=a';
+
 
   // Append the script to the document body
   document.body.appendChild(script);
 });
+
